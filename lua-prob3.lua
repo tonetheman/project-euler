d876c9873bf81b718a7c60e7dc4fa44c6fa12749
tonetheman <tonetheman@gmail.com>
2014-01-14 20:06:27 -0600

    added check for prime function

--- lua-prob3.lua
+++ lua-prob3.lua
@@ -2,6 +2,32 @@
 
 local utils = require("utils")
 local target = 600851475143
+local debug = false
+
+function is_prime(n)
+	if n == 1 or n == 2 then 
+		return true 
+	end
+	if n%2==0 then
+		return false
+	end
+	local check_to = math.sqrt(n)
+	if debug then
+		print("\tchecking to this val: " .. check_to)
+	end
+	local _res = false
+	for i=3,check_to,2 do
+		local val = n % i
+		if debug then
+			print("\tneed to check this factor: " .. i .. " " .. val)
+		end
+		if val == 0 then
+			return false
+		end
+	end
+	return true
+end
+
 
 function factor(t)
 	local top = math.sqrt(target)
@@ -17,4 +43,13 @@ function factor(t)
 	utils.pt(nums)
 end
 
-factor(target)
+function prime_test()
+	for i=1,100 do
+		if is_prime(i) then
+			print(i .. " PRIME")
+		end
+	end
+end
+
+prime_test()
+-- print(is_prime(9))
