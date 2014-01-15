84551604ae79709a5a5a60363bb2b156c939d881
tonetheman <tonetheman@gmail.com>
2014-01-15 11:16:54 -0600

    moved is prime to utils since i have been using it more

--- lua-prob10.lua
+++ lua-prob10.lua
@@ -1,29 +1,11 @@
 -- prob 10
 
-function is_prime(n)
-	if n == 1 then
-		return false
-	end
-	if n == 2 then 
-		return true 
-	end
-	if n%2==0 then
-		return false
-	end
-	local check_to = math.sqrt(n)
-	local _res = false
-	for i=3,check_to,2 do
-		local val = n % i
-		if val == 0 then
-			return false
-		end
-	end
-	return true
-end
+local utils = require("utils")
+
 
 local total = 0
 for i=1,2000000 do
-	if is_prime(i) then
+	if utils.is_prime(i) then
 		total = total + i
 	end
 end
--- utils.lua
+++ utils.lua
@@ -8,5 +8,26 @@ function me.pt(t)
 	end
 end
 
+function me.is_prime(n)
+	if n == 1 then
+		return false
+	end
+	if n == 2 then
+		return true
+	end
+	if n%2==0 then
+		return false
+	end
+	local check_to = math.sqrt(n)
+	local _res = false
+	for i=3,check_to,2 do
+		local val = n % i
+		if val == 0 then
+			return false
+		end
+	end
+	return true
+end
+
 
 return me
