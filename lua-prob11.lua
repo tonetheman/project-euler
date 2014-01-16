1ae24bdd7e004cf4306f76a062d55993d0176875
tonetheman <tonetheman@gmail.com>
2014-01-16 17:52:56 -0600

    added one diagonal l to r

--- lua-prob11.lua
+++ lua-prob11.lua
@@ -18,6 +18,13 @@ for line in io.lines("prob11.data") do
 	table.insert(data, tmp_data)
 end
 
+for i=1,20 do
+	for j=1,20 do
+		io.write(string.format("%2d ", data[i][j]))
+	end
+	print()
+end
+
 local gd = {}
 function gd.set(self,a,b,c,d)
 	self.a = a
@@ -26,8 +33,13 @@ function gd.set(self,a,b,c,d)
 	self.d = d
 end
 local guess = 0
+local guess_i = 0
+local guess_j = 0
+local guess_direction = "NONE"
 function pg()
 	print("guess: " .. guess)
+	print("i,j : " .. guess_i .. " " .. guess_j)
+	print("dir : " .. guess_direction)
 	print(string.format("%d %d %d %d", gd.a, gd.b, gd.c, gd.d))
 end
 
@@ -38,15 +50,38 @@ for i=1,20 do
 		local tmp = data[i][j] * data[i][j+1] * data[i][j+2] * data[i][j+3]
 		if tmp > guess then
 			guess = tmp
+			guess_i = i
+			guess_j = j
+			guess_direction = "L_TO_R"
 			gd:set(data[i][j],data[i][j+1],data[i][j+2],data[i][j+3])
 		end
 
 		tmp = data[j][i] * data[j+1][i] * data[j+2][i] * data[j+3][i]
 		if tmp > guess then
 			guess = tmp
+			guess_i = i
+			guess_j = j
+			guess_direction = "U_TO_D"
 			gd:set(data[j][i],data[j][i+1],data[j][i+2],data[j][i+3])
 		end
 	end
 end
 
+-- diagonal down
+for i=1,17 do
+	for j=1,17 do
+		local tmp = data[i][j]*data[i+1][j+1]*data[i+2][j+2]*data[i+3][j+3]
+		if tmp>guess then
+			guess = tmp
+			guess_i = i
+			guess_j = j
+			guess_direction = "DD"
+			gd:set(data[i][j],data[i+1][j+1],data[i+2][j+2],data[i+3][j+3])
+		end
+	end
+end
+
 pg()
+
+
+
