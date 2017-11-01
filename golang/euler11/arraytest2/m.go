75b2bd02fbfec7b15679c8b08e20773cb1a123c8
Tony Colston <tonetheman@gmail.com>
2017-11-01 16:21:27 -0500

    some comments

--- golang/euler11/arraytest2/m.go
+++ golang/euler11/arraytest2/m.go
@@ -7,29 +7,30 @@ import (
 
 func main() {
 
-
+	// string here with embedded newlines
         s := `08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00`
+
+	//
 	fmt.Println(s)
-	//d := strings.Split(s," ")
+
+	// magic here splits on whitespace!
 	d := strings.Fields(s)
 
 	// fix data
+	// need to see if there is another choice here
+	// to interpret strings as int with a radix
 	for i:=0;i<len(d);i++ {
-		// d[i] = strings.Trim(d[i],"\n\t ")
-		fmt.Println(d[i], []byte(d[i]))
-		/*
 		if d[i] == "00" {
 			d[i] = "0"
 		}
 		if strings.HasPrefix(d[i],"0") && len(d[i]) == 2  {
 			fmt.Println("FIX",d[i], len(d[i]), d[i][1:])
 			d[i] = d[i][1:]
-		} else {
-			fmt.Println("NOT FIX", d[i], len(d[i]))
-		}
-		*/
+		} 
 	}
 
 	fmt.Println(len(d))
+	fmt.Println(d)
+
 }
