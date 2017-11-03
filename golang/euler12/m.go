731fc7633183a4fdb1b0298956733168cf960e94
tonetheman <tonetheman@gmail.com>
2017-11-03 07:22:53 -0500

    finished 12

--- golang/euler12/m.go
+++ golang/euler12/m.go
@@ -6,27 +6,23 @@ import "math"
 const tri1 int64 = 1
 var current_tri int64 = 1
 
-var facs [600]int64
-func clear_facs() {
-  for i:=0;i<500;i++ {
-    facs[i] = 0
-  }
-}
-
 func factors(n int64) int {
   var i int64
-  clear_facs()
-  facs[0] = 1
-  facs[1] = n
-  var counter int = 2
+
+  // we have 2 factors 1, n
+  var count int = 2
+
+  // now check up to sqrt
+  // each factor you find gives you 2 more
+  // the number you found plus the matching number
+  // above the sqrt
   for i=2;float64(i)<math.Sqrt(float64(n))+1;i++ {
     if n%i == 0 {
-      facs[counter]=i
-      counter = counter + 1
+      count = count + 2
     }
   }
   //fmt.Println(n,facs)
-  return counter
+  return count
 }
 
 func main() {
@@ -35,8 +31,9 @@ func main() {
   for i = 2;i<100000;i++ {
     current_tri = current_tri + i
     counter := factors(current_tri)
-    if counter >400 {
+    if counter >500 {
       fmt.Println("current triangle number", current_tri, counter)
+      return
     }
   }
 }
