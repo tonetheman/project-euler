b1f002d80f9877c4969e9679d952ae8b3dbba747
tonetheman <tonetheman@gmail.com>
2017-11-08 07:32:53 -0600

    looking at data

--- golang/euler54/p.go
+++ golang/euler54/p.go
@@ -4,6 +4,7 @@ import (
   "io/ioutil"
   "fmt"
   "strings"
+  "sort"
 )
 const filename string = "poker.txt"
 
@@ -17,5 +18,15 @@ func main() {
   data := strings.Split(string(content),"\n")
   fmt.Println(data[1000]) // this one is empty
   fmt.Println("len",len(data))
+
+  fmt.Println()
+  fmt.Println(data[0])
+  d := strings.Split(data[0]," ")
+  var hand0 [5]string
+  for i:=0;i<5;i++ {
+    hand0[i] = d[i]
+  }
+  sort.Strings(hand0[:])
+  fmt.Println(hand0)
 
 }
