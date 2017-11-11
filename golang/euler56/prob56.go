6a748906e38856b83a6c725682f79007d08482e2
tonetheman <tonetheman@gmail.com>
2017-11-10 23:16:09 -0600

    done

--- golang/euler56/prob56.go
+++ golang/euler56/prob56.go
@@ -3,22 +3,42 @@ package main
 import (
   "fmt"
   "math/big"
+  "strconv"
 )
 
 func SumDigits(n big.Int) int {
-
-  return 0
+  s := n.String()
+  sum := 0
+  for i:=0;i<len(s);i++ {
+    c := string(s[i])
+    tmpi,err := strconv.Atoi(c)
+    if err!=nil {
+      fmt.Println("ERR",err)
+    }
+    sum += tmpi
+  }
+  return sum
 }
 
 func main() {
-  for i:=1;i<10;i++ {
-    for j:=1;j<10;j++ {
+  max_i := -1
+  max_j := -1
+  max_value := -1
+
+  for i:=1;i<100;i++ {
+    for j:=1;j<100;j++ {
       x := big.NewInt(int64(i))
       y := big.NewInt(int64(j))
       var z big.Int
       z.Exp(x,y,nil)
-      SumDigits(z)
-      fmt.Println(z.String())
+      tots := SumDigits(z)
+      if tots > max_value {
+        max_i = i
+        max_j = j
+        max_value = tots
+      }
     }
   }
+  fmt.Printf("max i and max j and max value: %d %d %d\n",max_i,max_j,max_value)
+
 }
