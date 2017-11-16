4e94e169ef4028f09b056c4b87cbe2fb7aff7281
tonetheman <tonetheman@gmail.com>
2017-11-16 07:20:17 -0600

    solved the wrong problem

--- golang/euler19/m.go
+++ golang/euler19/m.go
@@ -1,72 +1,27 @@
 package main
 
-import "fmt"
-
-const (
-  MON = 1
-  TUE = 2
-  WED = 3
-  THU = 4
-  FRI = 5
-  SAT = 6
-  SUN = 7
+import (
+	"fmt"
+	"time"
 )
-
-const (
-  JAN = 1
-  FEB = 2
-  MAR = 3
-  APR = 4
-  MAY = 5
-  JUN = 6
-  JUL = 7
-  AUG = 8
-  SEP = 9
-  OCT = 10
-  NOV = 11
-  DEC = 12
-)
-
-var DAY_0 = MON // 1 Jan 1900 is monday
-
-var month_day_counts map[int]int
-
-func IsLeapYear(n int) bool {
-  var res = n%4==0
-
-  if n%100==0 {
-    res = false
-    if n%400 == 0 {
-      res = true
-    }
-  }
-  return res
-}
-
-func FillMonths(n int) {
-    month_day_counts[JAN] = 31
-    month_day_counts[FEB] = 28
-    if IsLeapYear(n) {
-      month_day_counts[FEB] = 29
-    }
-    month_day_counts[MAR] = 31
-    month_day_counts[APR] = 30
-    month_day_counts[MAY] = 31
-    month_day_counts[JUN] = 30
-    month_day_counts[JUL] = 31
-    month_day_counts[AUG] = 21
-    month_day_counts[SEP] = 30
-    month_day_counts[OCT] = 31
-    month_day_counts[NOV] = 30
-    month_day_counts[DEC] = 31
-}
 
 func main() {
-  fmt.Println(1)
-  month_day_counts = make(map[int]int)
-
-  for i := 1901;i<1910;i++ {
-    FillMonths(i)
-    fmt.Println("Year",i,IsLeapYear(i))
-  }
+	fmt.Println(time.January)
+	locl, err := time.LoadLocation("UTC")
+	if err != nil {
+		fmt.Println(err)
+	}
+	sundayCount := 0
+	// this is the years
+	for i := 1901; i < 2001; i++ {
+		// this is the days
+		for j := 1; j < 32; j++ {
+			d := time.Date(i, time.January, j, 0, 0, 0, 0, locl)
+			fmt.Println(d, d.Day(), d.Weekday())
+			if d.Weekday() == time.Sunday {
+				sundayCount++
+			}
+		}
+	}
+	fmt.Println("sunday count", sundayCount)
 }
