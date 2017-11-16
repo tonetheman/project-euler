4dba4247cf62ed89f09ab372f6cb69521cb7f644
tonetheman <tonetheman@gmail.com>
2017-11-16 06:39:15 -0600

    finished with 17

--- golang/euler17/prog.go
+++ golang/euler17/prog.go
@@ -4,68 +4,79 @@ import "fmt"
 
 var simple map[int]string
 
+// ConvertNumber takes and int and returns a
+// string version of the spoken number
 func ConvertNumber(n int) string {
-    if n<=20 {
-      return simple[n]
-    }
-    if n>20 && n<100 {
-      // compose it
-      digit_one := n%10
-      leading_digit := n/10 * 10
-      return simple[leading_digit] + " " + simple[digit_one]
-    }
-    if n>=100 {
-      ms_digit := n/100
-      tens := n%100
-      var last string = ""
-      if tens <=20 {
-        last = simple[tens]
-      } else {
-        digit_one := tens%10
-        leading_digit := tens/10 * 10
-        last = simple[leading_digit] + " " + simple[digit_one]
-      }
-      return simple[ms_digit] + " hundred and " + last
-    }
-    return "UNK"
+	if n <= 20 {
+		return simple[n]
+	}
+	if n == 1000 {
+		return simple[n]
+	}
+	if n > 20 && n < 100 {
+		// compose it
+		digitOne := n % 10
+		leadingDigit := n / 10 * 10
+		return simple[leadingDigit] + "" + simple[digitOne]
+	}
+	if n >= 100 {
+		msDigit := n / 100
+		tens := n % 100
+		var last string
+		if tens <= 20 {
+			last = simple[tens]
+		} else {
+			digitOne := tens % 10
+			leadingDigit := tens / 10 * 10
+			last = simple[leadingDigit] + "" + simple[digitOne]
+		}
+		if n%100 == 0 {
+			return simple[msDigit] + "hundred"
+		}
+		return simple[msDigit] + "hundredand" + last
+
+	}
+	return "UNK"
 }
-func FillOutSimple() {
-  simple = make(map[int]string)
-  simple[1] = "one"
-  simple[2] = "two"
-  simple[3] = "three"
-  simple[4] = "four"
-  simple[5] = "five"
-  simple[6] = "six"
-  simple[7] = "seven"
-  simple[8] = "eight"
-  simple[9] = "nine"
-  simple[10] = "ten"
-  simple[11] = "eleven"
-  simple[12] = "twelve"
-  simple[13] = "thirteen"
-  simple[14] = "fourteen"
-  simple[15] = "fifteen"
-  simple[16] = "sixteen"
-  simple[17] = "seventeen"
-  simple[18] = "eighteen"
-  simple[19] = "nineteen"
-  simple[20] = "twenty"
-  simple[30] = "thirty"
-  simple[40] = "forty"
-  simple[50] = "fifty"
-  simple[60] = "sixty"
-  simple[70] = "seventy"
-  simple[80] = "eighty"
-  simple[90] = "ninety"
+func fillOutSimple() {
+	simple = make(map[int]string)
+	simple[1] = "one"
+	simple[2] = "two"
+	simple[3] = "three"
+	simple[4] = "four"
+	simple[5] = "five"
+	simple[6] = "six"
+	simple[7] = "seven"
+	simple[8] = "eight"
+	simple[9] = "nine"
+	simple[10] = "ten"
+	simple[11] = "eleven"
+	simple[12] = "twelve"
+	simple[13] = "thirteen"
+	simple[14] = "fourteen"
+	simple[15] = "fifteen"
+	simple[16] = "sixteen"
+	simple[17] = "seventeen"
+	simple[18] = "eighteen"
+	simple[19] = "nineteen"
+	simple[20] = "twenty"
+	simple[30] = "thirty"
+	simple[40] = "forty"
+	simple[50] = "fifty"
+	simple[60] = "sixty"
+	simple[70] = "seventy"
+	simple[80] = "eighty"
+	simple[90] = "ninety"
+	simple[1000] = "onethousand"
 }
 
 func main() {
-  FillOutSimple()
-  //for i := 1;i<100;i++ {
-  //  fmt.Println(ConvertNumber(i))
-//
-//  }
-  fmt.Println(ConvertNumber(109))
-  fmt.Println(ConvertNumber(567))
+	fillOutSimple()
+	total := 0
+	for i := 1; i < 1001; i++ {
+		ts := ConvertNumber(i)
+		fmt.Println(ts, len(ts))
+		total += len(ts)
+	}
+	fmt.Println("total", total)
 }
