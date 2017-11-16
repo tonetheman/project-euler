accc07c8ee7f8f1bb8049aed19a163788acff767
tonetheman <tonetheman@gmail.com>
2017-11-16 07:27:54 -0600

    done with 19

--- golang/euler19/m.go
+++ golang/euler19/m.go
@@ -12,16 +12,19 @@ func main() {
 		fmt.Println(err)
 	}
 	sundayCount := 0
-	// this is the years
-	for i := 1901; i < 2001; i++ {
-		// this is the days
-		for j := 1; j < 32; j++ {
-			d := time.Date(i, time.January, j, 0, 0, 0, 0, locl)
-			fmt.Println(d, d.Day(), d.Weekday())
-			if d.Weekday() == time.Sunday {
-				sundayCount++
-			}
+	// starting point
+	d := time.Date(1901, time.January, 1, 0, 0, 0, 0, locl)
+	// ending point
+	topEnd := time.Date(2001, time.January, 1, 0, 0, 0, 0, locl)
+
+	for d.Before(topEnd) {
+
+		fmt.Println(d, d.Day(), d.Weekday())
+		if d.Weekday() == time.Sunday && d.Day() == 1 {
+			sundayCount++
 		}
+
+		d = d.Add(time.Duration(24 * time.Hour))
 	}
 	fmt.Println("sunday count", sundayCount)
 }
