54d9606e577b35b05e5260c7e4555f5dd8e497b8
tonetheman <tonetheman@gmail.com>
2017-11-25 17:39:01 -0600

    added create of hand using card struct

--- golang/euler54/p.go
+++ golang/euler54/p.go
@@ -28,7 +28,78 @@ type Hand struct {
 	h [5]string
 }
 
+func translateRank(s string) int {
+	var c = s[0]
+	if c == 'A' {
+		return 1
+	}
+	if c == '2' {
+		return 2
+	}
+	if c == '3' {
+		return 3
+	}
+	if c == '4' {
+		return 4
+	}
+	if c == '5' {
+		return 5
+	}
+	if c == '6' {
+		return 6
+	}
+	if c == '7' {
+		return 7
+	}
+	if c == '8' {
+		return 8
+	}
+	if c == '9' {
+		return 9
+	}
+	if c == 'T' {
+		return 10
+	}
+	if c == 'J' {
+		return 11
+	}
+	if c == 'Q' {
+		return 12
+	}
+	if c == 'K' {
+		return 13
+	}
+	return -1
+}
+
+func translateSuit(s string) int {
+	if s[1] == 'C' {
+		return 0
+	}
+	if s[1] == 'S' {
+		return 1
+	}
+	if s[2] == 'D' {
+		return 2
+	}
+	if s[3] == 'H' {
+		return 3
+	}
+	return -1
+}
+
+func newHand(h []string) [5]Card {
+	var cards [5]Card
+	cards[0] = Card{translateRank(h[0]), translateSuit(h[0])}
+	cards[1] = Card{translateRank(h[1]), translateSuit(h[1])}
+	cards[2] = Card{translateRank(h[2]), translateSuit(h[2])}
+	cards[3] = Card{translateRank(h[3]), translateSuit(h[3])}
+	cards[4] = Card{translateRank(h[4]), translateSuit(h[4])}
+	return cards
+}
+
 func TransformHand(h []string) {
+	fmt.Println("orighand", h)
 	// change TJQK in rank
 	// TO WXYZ so they will sort like we want
 	for i := 0; i < 5; i++ {
